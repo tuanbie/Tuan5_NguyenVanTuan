6a04d7f4444a1b0b15752d5808cdc304aa9c9722
tuanbie <tuansk1002@gmail.com>
2022-06-16 20:18:55 +0700

    Xong đăng nhập, đăng kí

---
+++ Tuan5_NguyenVanTuan/Controllers/NguoiDungController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tuan5_NguyenVanTuan.Models;
+
+namespace Tuan5_NguyenVanTuan.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        [HttpGet]
+        // GET: NguoiDung
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangKy(FormCollection collection, KhachHang kh)
+        {
+            //
+            var hoten = collection["hoten"];
+            var tendangnhap = collection["tendangnhap"];
+            var matkhau = collection["matkhau"];
+            var MatKhauXacNhan = collection["MatKhauXacNhan"];
+            var email = collection["email"];
+            var diachi = collection["Address"];
+            var dienthoai = collection["dienthoai"];
+            var ngaysinh = String.Format("{0:MM/dd/yyyy}", collection["ngaysinh"]);
+            if (String.IsNullOrEmpty(MatKhauXacNhan))
+            {
+                ViewData["NhapMKXN"] = "Phải nhập mật khẩu xác nhận !";
+            }
+            else
+            {
+
+            
+               if(!matkhau.Equals(MatKhauXacNhan))
+                {
+                    ViewData["MatKhauGiongNhau"] = "Mật khẩu và mật khẩu xác nhận phải giống nhau!";
+                }
+                else
+                {
+                    kh.hoten = hoten;
+                    kh.tendangnhap = tendangnhap;
+                    kh.matkhau = matkhau;
+                    kh.email = email;
+                    kh.diachi = diachi;
+                    kh.dienthoai = dienthoai;
+                    kh.ngaysinh = DateTime.Parse(ngaysinh);
+                    data.KhachHangs.InsertOnSubmit(kh);
+                    data.SubmitChanges();
+                    return RedirectToAction("Login", "NguoiDung");
+                }
+            
+            }
+            return this.DangKy();
+        }
+
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection collection)
+        {
+            string tendangnhap = collection["tendangnhap"];
+            string matkhau = collection["matkhau"];
+            KhachHang kh = data.KhachHangs.SingleOrDefault(a => a.tendangnhap == tendangnhap && a.matkhau == matkhau);
+            if (kh != null)
+            {
+                Session["Taikhoan"] = kh;
+                ViewBag.ThongBao = "Chúc mừng đăng nhập thành công!";
+            }
+            else
+            {
+                ViewBag.Thongbao = "Tên Tài Khoản Hoặc Mật Khẩu Không Đúng";
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult LogOut()
+        {
+            Session["Taikhoan"] = null;
+            return RedirectToAction("DangNhap", "NguoiDung");
+        }
+    }
+}
